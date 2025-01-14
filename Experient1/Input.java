92f7dd53e244ce8e9f988016071a643453dcbb88
Girija Shankar ray <91793906+Girijaray07@users.noreply.github.com>
2025-01-14 23:20:09 +0530

    Complete Input Methods

---
+++ Experient1/Input.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main (String args[])
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a INT value: ");
+        int num = input.nextInt();
+        System.out.println("Value: " + num);
+
+        System.out.print("Enter FLOAT Value: ");
+        float num1 = input.nextFloat();
+        System.out.println("Value: " + num1);
+
+        input.nextLine();
+
+        System.out.print("Enter STRING Value: ");
+        String num2 = input.nextLine();
+        System.out.println("Value: " + num2);
+
+        System.out.print("Enter BOOLEAN Value: ");
+        Boolean num3 = input.nextBoolean();
+        System.out.println("Value: " + num3);
+    }
+}
