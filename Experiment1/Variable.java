b6b65e2686c96b3a64adc56850bfbba606aa1dba
Girija Shankar ray <91793906+Girijaray07@users.noreply.github.com>
2025-01-19 12:01:12 +0530

    Complete Instance and Local Variable

---
+++ Experiment1/Variable.java
@@ -0,0 +1,12 @@
+public class Variable 
+{
+    int number;  //instance variable
+    public static void main (String args[])
+    {
+        int num = 10;  //local variable
+
+        Variable T = new Variable();
+        
+        System.out.println("Local Variable: "+ num + "\nInstance Variable: "+ T.number);
+    }
+}
