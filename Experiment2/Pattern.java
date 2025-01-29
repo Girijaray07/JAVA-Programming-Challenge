f479651e90861fe3da57a884aca1c19cef826eda
Girija Shankar ray <91793906+Girijaray07@users.noreply.github.com>
2025-01-29 20:53:32 +0530

    Pattern Problem

---
+++ Experiment2/Pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of levels: ");
+        int levels = sc.nextInt();
+
+        System.out.println("Pattern 1:");
+        for (int i = 1; i <= levels; i++) {
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPattern 2:");
+        for (int i = 1; i <= levels; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
