b64e83d3f405eed99e164b935973d227ac409122
Girija Shankar ray <91793906+Girijaray07@users.noreply.github.com>
2025-02-18 00:30:41 +0530

    Complete Use of Access Modifier

---
+++ LAB4/AccessModifier.java
@@ -0,0 +1,22 @@
+class Parent {
+    protected int protectedVar = 10; 
+
+    protected void protectedMethod() {
+        System.out.println("This is a protected method");
+    }
+}
+
+class Child extends Parent {
+    public void display() {
+        System.out.println("Accessing Protected Variable: " + protectedVar);
+        protectedMethod();
+    }
+}
+
+public class AccessModifier {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.display();
+
+    }
+}
