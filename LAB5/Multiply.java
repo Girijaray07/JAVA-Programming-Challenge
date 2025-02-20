b39218cf5f152b4505a2f1b33856cc8daee0c732
Girija Shankar ray <91793906+Girijaray07@users.noreply.github.com>
2025-02-20 15:08:35 +0530

    Complete Multiple Java Program

--- LAB5/Multiply.java
+++ LAB5/Multiply.java
@@ -1,31 +1,51 @@
-public class Multiply
-{
+import java.util.Scanner;
+
+public class Multiply {
     void multiply(int a, int b) {
-        System.out.printf("The product of %d and %d is: %d", a, b, a*b);
+        System.out.printf("Multiple of %d and %d is: %d\n", a, b, a * b);
     }
 
     void multiply(int a, int b, int c) {
-        System.out.printf("The product of %d, %d and %d is: %d", a, b, c, a*b*c);
+        System.out.printf("Multiple of %d, %d and %d is: %d\n", a, b, c, a * b * c);
     }
 
     void multiply(int arr[]) {
         int res = 1;
-        for(int num: arr) {
+        for (int num : arr) {
             res *= num;
         }
-        System.out.printf("The Multiple of the numbers is: %d", res);
+        System.out.printf("Result: %d\n", res);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numbers[] = new int[50];
+        int size = 0;
+
+        while (true) {
+            System.out.print("Enter numbers (Press * to stop): ");
+            if (input.hasNextInt()) {
+                numbers[size] = input.nextInt();
+                size++;
+            } else {
+                String store = input.next();
+                if (store.equals("*")) {
+                    break;
+                }
+            }
+        }
+        input.close();
+
         Multiply obj = new Multiply();
-        obj.multiply(2, 3);
+        int[] finalNumbers = new int[size];
+        System.arraycopy(numbers, 0, finalNumbers, 0, size);
 
-        System.out.println();
-        obj.multiply(2, 3, 4); 
-
-        System.out.println();
-        int arr[] = {2, 3, 4, 5};
-        obj.multiply(arr); 
+        if (size == 2) {
+            obj.multiply(finalNumbers[0], finalNumbers[1]);
+        } else if (size == 3) {
+            obj.multiply(finalNumbers[0], finalNumbers[1], finalNumbers[2]);
+        } else {
+            obj.multiply(finalNumbers);
+        }
     }
 }
