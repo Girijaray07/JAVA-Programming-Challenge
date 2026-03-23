85a4704b0c63d101532356caa033cc692c1c33a5
Girija Ray <91793906+Girijaray07@users.noreply.github.com>
2026-03-24 00:17:40 +0530

    Practice Arrays

---
+++ arraysPractice/ArrayListPractice.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        arr.add(67);
+        arr.add(67);
+        arr.add(67);
+        arr.add(67);
+        arr.add(67);
+        arr.add(67);
+
+        System.out.println(arr.get(4));
+    }    
+}
---
+++ arraysPractice/Arrays1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5,6,7,8,9};
+        int[] arr2 = new int[10];
+
+        int[][][] arr = new int[5][5][5];
+        String[] arr3 = new String[10];
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr[0][0]));
+    }
+}
