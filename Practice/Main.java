331a39e3d7dcad50fcc4a4aebf4acb974c667451
Girija Ray <91793906+Girijaray07@users.noreply.github.com>
2026-03-25 10:21:37 +0530

    Main Class in JAVA

---
+++ Practice/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){        
+        System.out.println("こんにちは Girija");
+
+        int num = (int)23.45f;
+        System.out.println(num+"hello");
+    }
+}
