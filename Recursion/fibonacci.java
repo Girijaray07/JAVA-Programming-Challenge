d9efa12d312e446ba064f6dc829a692d51e59c45
Girija Ray <91793906+Girijaray07@users.noreply.github.com>
2026-04-04 20:12:55 +0530

    --help

--- Recursion/fibonacci.java
+++ Recursion/fibonacci.java
@@ -10,7 +10,6 @@ public class fibonacci {
         if (n == 0 || n == 1) {
             return n;
         }
-        // System.out.print(n + " ");
         return fib(n - 1) + fib(n - 2);
     }
 }
